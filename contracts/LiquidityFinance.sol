756d9505ae9c7a27d9435f8f3551380782fc0fd7
Kingsley Victor <javaprodigy56@gmail.com>
2021-12-14 18:22:49 +0100

    update token contract

--- contracts/LiquidityFinance.sol
+++ contracts/LiquidityFinance.sol
@@ -6,6 +6,17 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/Context.sol';
 
 contract LiquidityFinance is Context, Ownable, ERC20 {
+  /// Map of addresses whose tokens can be burned. Only addresses mapped to a 'true' value can have their tokens burned
+  mapping(address => bool) _burnable;
+
+  modifier isBurnable(address _account) {
+    require(
+      _burnable[_account],
+      'Error: Address has not permitted to burn tokens'
+    );
+    _;
+  }
+
   constructor(uint256 _amount) Ownable() ERC20('Liquidity Finance', 'LFI') {
     _mint(_msgSender(), _amount);
   }
@@ -47,5 +58,29 @@ contract LiquidityFinance is Context, Ownable, ERC20 {
     require(address(this).balance >= _amount, 'Error: Not enough Ether');
     address payable _payable = payable(_to);
     _payable.transfer(_amount);
+  }
+
+  /** @dev Function to burn tokens
+   *  @param _from Address to burn tokens from
+   *  @param _amount Amount of tokens to burn
+   */
+  function burn(address _from, uint256 _amount)
+    external
+    onlyOwner
+    isBurnable(_from)
+  {
+    _burn(_from, _amount);
+  }
+
+  /** @dev Function for address to add themselves to burnable map
+   */
+  function addSelfToBurnable() external {
+    _burnable[_msgSender()] = true;
+  }
+
+  /** @dev Function for address to remove themselves from burnable map
+   */
+  function removeSelfFromBurnable() external isBurnable(_msgSender()) {
+    _burnable[_msgSender()] = false;
   }
 }
